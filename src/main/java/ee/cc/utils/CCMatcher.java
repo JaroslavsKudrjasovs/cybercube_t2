394942ecd529658f4661da7d737df18a5b4e3152
jaroslav <jar@inbox.lv>
2024-04-05 09:43:24 +0300

    modified test output

--- src/main/java/ee/cc/utils/CCMatcher.java
+++ src/main/java/ee/cc/utils/CCMatcher.java
@@ -3,10 +3,13 @@ package ee.cc.utils;
 import org.apache.logging.log4j.Logger;
 import org.hamcrest.Matcher;
 import org.hamcrest.MatcherAssert;
+import org.testng.Reporter;
 
 public class CCMatcher {
     public static <T> void assertThat(Logger logger, String reason, T actual, Matcher<? super T> matcher) {
         MatcherAssert.assertThat(reason, actual, matcher);
-        logger.info("Successfully matched actual '" + actual + "' with expected " + matcher);
+        String message = reason + " Successfully matched actual '" + actual + "' with expected " + matcher;
+        logger.info(message);
+        Reporter.log(message);
     }
 }
